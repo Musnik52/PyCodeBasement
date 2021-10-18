79a80a2e3161b3b1e04d11c574c00f395373484f
Boris <musnik52@gmail.com>
2021-10-18 18:10:52 +0300

    Create random_bingo_limited.py

---
+++ random_bingo_limited.py
@@ -0,0 +1,20 @@
+import random
+
+def game(round):
+    num = random.randint(1,100)
+    guess = 1
+    unum = int(input(f"Welcome to round {round+1} of Rando-Bingo! Guess the lucky number: "))
+    while unum != num:
+        if unum < num: print('Too low!')
+        else: print('Too high!')
+        guess += 1
+        unum = int(input('Try again: '))
+    print(f'Rando - BINGO! You had to guess {guess} times.')
+    return guess
+
+y = 0
+for i in range(3):
+    x = game(i)
+    if y == 0: y = x
+    if x < y: y = x
+print(f'The least number of guesses was: {y}')
