52371f64c2dab7add4882b43b882335ab99f4839
Boris <musnik52@gmail.com>
2021-11-22 08:46:14 +0200

    Create sql_import.py

---
+++ sql_import.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+class Employee:
+    def __init__(self, id, name, age, address, salary):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.address = address
+        self.salary = salary
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+conn = sqlite3.connect('C:\git\pyCodeBasement\SQL\\1test.db')
+print('connection to db opened')
+
+cursor = conn.execute('SELECT*FROM COMPANY')
+_emp = []
+for row in cursor:
+    e = Employee(row[0], row[1], row[2], row[3], row[4])
+    _emp.append(e)
+print(_emp)
